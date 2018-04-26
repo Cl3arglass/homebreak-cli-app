74d4d8ab757825e897490474968baa81d48fe38b
Holden Altaffer <haltaffer@Holdens-MacBook-Pro.local>
2018-04-26 14:11:27 -0700

    further defined break.rb with methods that will hold data for each beach

--- lib/home_break/break.rb
+++ lib/home_break/break.rb
@@ -8,13 +8,32 @@ class HomeBreak::Break
     # puts "3. Pacifica/Lindamar"
     # now this method needs to return an array of objects
 
-    break_1 = self.new
-    break_1.name = "Ocean Beach Overview"
-    break_1.height = "5-7 ft"
-    break_1.period = "11s"
-    break_1.wind = "9 kts WSW (257˚)"
-    break_1.tide = "3.0ft"
+   self.scrape_breaks
+  end
 
+  def self.scrape_breaks
+    breaks = []
+
+    breaks << self.scrape_OBO
+    breaks << self.scrape_SOB
+    breaks << self.scrape_PCL
+
+    breaks
+  end
+
+  def self.scrape_OBO
+    # doc = Nokogiri::HTML(open(http://www.surfline.com/surf-report/ocean-beach-overview-northern-california_4127/))
+    break_1 = self.new
+     break_1.name = "Ocean Beach Overview"
+     break_1.height = "5-7 ft"
+     break_1.period = "11s"
+     break_1.wind = "9 kts WSW (257˚)"
+     break_1.tide = "3.0ft"
+
+     break_1
+  end
+
+  def self.scrape_SOB
     break_2 = self.new
     break_2.name = "South Ocean Beach"
     break_2.height = "5-6 ft"
@@ -22,6 +41,10 @@ class HomeBreak::Break
     break_2.wind = "8 kts WSW (256˚)"
     break_2.tide = "2.0ft"
 
+    break_2
+  end
+
+  def self.scrape_PCL
     break_3 = self.new
     break_3.name = "Pacifica/Lindamar"
     break_3.height = "4-5 ft"
@@ -29,7 +52,7 @@ class HomeBreak::Break
     break_3.wind = "7 kts WSW (246˚)"
     break_3.tide = "1.0ft"
 
-   [break_1, break_2, break_3]
+    break_3
   end
 
 
